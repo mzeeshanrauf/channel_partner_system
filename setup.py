13c6cce3698b99629f17c961bb6838ab2f5042c5
mzeeshanrauf <zeeshanrauf@outlook.com>
2026-03-31 22:07:31 +0300

    fixes

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
 from setuptools import setup, find_packages
-setup(name="channel_partner_system", version="0.0.1", packages=find_packages())
+
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
+setup(
+    name="channel_partner_system",
+    version="0.0.1",
+    description="Channel Partner System",
+    author="Your Company",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
+)
